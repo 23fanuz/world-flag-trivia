d7d10964869e654b043691c3e50f61f2b970cca0
23fanuz <fanuzstephanie@gmail.com>
2024-10-15 16:25:56 -0400

    moved main activity to view pckg

--- app/src/main/java/com/sfanuz/worldflagtrivia/view/MainActivity.kt
+++ app/src/main/java/com/sfanuz/worldflagtrivia/view/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.sfanuz.worldflagtrivia
+package com.sfanuz.worldflagtrivia.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.sfanuz.worldflagtrivia.R
 
 
 class MainActivity : AppCompatActivity() {
