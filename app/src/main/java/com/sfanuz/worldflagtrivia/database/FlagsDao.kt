2123f4cdd3fa8ceafd47753ea6fe66931ff0dc1f
23fanuz <fanuzstephanie@gmail.com>
2024-10-21 13:48:05 -0400

    add: getThreeRandomRecords() function to retrieve three records from database that don't match the current ID

--- app/src/main/java/com/sfanuz/worldflagtrivia/database/FlagsDao.kt
+++ app/src/main/java/com/sfanuz/worldflagtrivia/database/FlagsDao.kt
@@ -27,4 +27,28 @@ class FlagsDao {
         return recordsList
 
     }
+
+
+    fun getRandomThreeRecords(helper: DatabaseCopyHelper, id : Int) : ArrayList<FlagsModel> {
+        val recordsList = ArrayList<FlagsModel>()
+        val database : SQLiteDatabase = helper.readableDatabase
+        val cursor : Cursor = database.rawQuery("SELECT * FROM flags WHERE flag_id != ? ORDER BY RANDOM() LIMIT 3", arrayOf(id.toString()))
+
+        val idIndex = cursor.getColumnIndex("flag_id")
+        val countryNameIndex = cursor.getColumnIndex("country_name")
+        val flagNameIndex = cursor.getColumnIndex("flag_name")
+
+        while (cursor.moveToNext()) {
+
+            val record = FlagsModel(cursor.getInt(idIndex), cursor.getString(countryNameIndex), cursor.getString(flagNameIndex))
+            recordsList.add(record)
+        }
+
+        cursor.close()
+        return recordsList
+
+    }
+
+
+
 }
