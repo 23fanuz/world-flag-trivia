3ba0c1dff29979ae4c6a9055aa5040ae7a625648
23fanuz <fanuzstephanie@gmail.com>
2024-10-21 15:11:28 -0400

    update: implemented showData()

--- app/src/main/java/com/sfanuz/worldflagtrivia/view/FragmentHome.kt
+++ app/src/main/java/com/sfanuz/worldflagtrivia/view/FragmentHome.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.sfanuz.worldflagtrivia.databinding.FragmentHomeBinding
+import com.techmania.flagquizwithsqlitedemo.DatabaseCopyHelper
 
 class FragmentHome : Fragment() {
 
@@ -19,12 +22,34 @@ lateinit var fragmentHomeBinding : FragmentHomeBinding
 
         fragmentHomeBinding = FragmentHomeBinding.inflate(inflater,container, false)
 
+        createAndOpenDatabase()
+
         fragmentHomeBinding.buttonStart.setOnClickListener {
+
+            //open new page when user clicks Start
+            val direction = FragmentHomeDirections.actionFragmentHomeToFragmentQuiz()
+            this.findNavController().navigate(direction)
+
 
         }
 
 
         return fragmentHomeBinding.root
+    }
+
+    private fun createAndOpenDatabase() {
+
+        //try-catch
+        try {
+
+            val helper = DatabaseCopyHelper(requireActivity())
+            helper.createDataBase()
+            helper.openDataBase()
+
+        } catch (e : Exception){
+
+            e.printStackTrace()
+        }
     }
 
 
--- app/src/main/java/com/sfanuz/worldflagtrivia/view/FragmentQuiz.kt
+++ app/src/main/java/com/sfanuz/worldflagtrivia/view/FragmentQuiz.kt
@@ -6,11 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sfanuz.worldflagtrivia.R
+import com.sfanuz.worldflagtrivia.database.FlagsDao
 import com.sfanuz.worldflagtrivia.databinding.FragmentQuizBinding
+import com.sfanuz.worldflagtrivia.model.FlagsModel
+import com.techmania.flagquizwithsqlitedemo.DatabaseCopyHelper
 
 class FragmentQuiz : Fragment() {
 
     lateinit var fragmentQuizBinding : FragmentQuizBinding
+
+    var flagList = ArrayList<FlagsModel>()
+
+    var correctNumber = 0
+    var wrongNumber = 0
+    var emptyNumber = 0
+    var questionNumber = 0
+
+    lateinit var correctFlag : FlagsModel
+    var wrongFlags = ArrayList<FlagsModel>()
+    val dao = FlagsDao()
 
 
     override fun onCreateView(
@@ -19,6 +33,12 @@ class FragmentQuiz : Fragment() {
     ): View? {
 
         fragmentQuizBinding = FragmentQuizBinding.inflate(inflater, container, false)
+
+
+        flagList = dao.getRandomData(DatabaseCopyHelper(requireActivity()))
+
+        showData()
+
 
         fragmentQuizBinding.buttonA.setOnClickListener {
 
@@ -41,6 +61,22 @@ class FragmentQuiz : Fragment() {
         }
 
         return fragmentQuizBinding.root
+    }
+
+    private fun showData() {
+
+        fragmentQuizBinding.textViewQuestion.text = resources.getString(R.string.question_number).plus(" ").plus(questionNumber + 1)
+
+        correctFlag = flagList[questionNumber]
+
+        fragmentQuizBinding.imageViewFlag.setImageResource(resources.getIdentifier(correctFlag.flagName, "drawable", requireActivity().packageName))
+
+        wrongFlags = dao.getRandomThreeRecords(DatabaseCopyHelper(requireActivity()),correctFlag.id)
+
+
+
+
+
     }
 
 }
