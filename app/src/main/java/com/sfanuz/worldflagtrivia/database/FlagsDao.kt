3a7cee1036a324b57cdf65628d62f77befa1a48c
23fanuz <fanuzstephanie@gmail.com>
2024-10-21 13:41:38 -0400

    add: flag DAO class implemented functions to read from db

---
+++ app/src/main/java/com/sfanuz/worldflagtrivia/database/FlagsDao.kt
@@ -0,0 +1,30 @@
+package com.sfanuz.worldflagtrivia.database
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import androidx.core.view.ContentInfoCompat.Flags
+import com.sfanuz.worldflagtrivia.model.FlagsModel
+import com.techmania.flagquizwithsqlitedemo.DatabaseCopyHelper
+
+class FlagsDao {
+
+    fun getRandomData(helper: DatabaseCopyHelper) : ArrayList<FlagsModel> {
+        val recordsList = ArrayList<FlagsModel>()
+        val database : SQLiteDatabase = helper.readableDatabase
+        val cursor : Cursor = database.rawQuery("SELECT * FROM flags ORDER BY RANDOM() LIMIT 10", null)
+
+        val idIndex = cursor.getColumnIndex("flag_id")
+        val countryNameIndex = cursor.getColumnIndex("country_name")
+        val flagNameIndex = cursor.getColumnIndex("flag_name")
+
+        while (cursor.moveToNext()) {
+
+            val record = FlagsModel(cursor.getInt(idIndex), cursor.getString(countryNameIndex), cursor.getString(flagNameIndex))
+            recordsList.add(record)
+        }
+
+        cursor.close()
+        return recordsList
+
+    }
+}
